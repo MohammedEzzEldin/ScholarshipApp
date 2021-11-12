c6c7fe319ba41836284e0469bec7f21a1c408eeb
mohammadezzeldin <35611711+mohammadezzeldin@users.noreply.github.com>
2021-11-12 21:11:50 +0200

    create a project

---
+++ ScholarShip/ScholarShip/Classes/ConstantVariables.cs
@@ -0,0 +1,14 @@
+﻿namespace ScholarShip.Classes
+{
+    public static class ConstantVariables
+    {
+        public const string adminsRole = "Admins";
+        public const string studentsRole = "Students";
+        public const string adminUserName = "admin";
+        public const string adminEamil = "mohammad_mahmoudezeldin@yahoo.com";
+        public const string adminDefaultPassword = "password";
+        public const int maxLengthOfPassword = 10;
+        public const int minLengthOfPassword = 6;
+        public const int lengthOfNationalID = 14;
+    }
+}
---
+++ ScholarShip/ScholarShip/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using ScholarShip.Models;
+using ScholarShip.Classes;
+
+[assembly: OwinStartupAttribute(typeof(ScholarShip.Startup))]
+namespace ScholarShip
+{
+    public partial class Startup
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateDefaultRoles();
+            CreateDefaultAdminUsers();
+        }
+
+        public void CreateDefaultRoles()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            IdentityRole role;
+
+            //add admins role
+            if (!roleManager.RoleExists(ConstantVariables.adminsRole))
+            {
+                role = new IdentityRole();
+                role.Name = ConstantVariables.adminsRole;
+                roleManager.Create(role);
+            }
+            // students role
+            if (!roleManager.RoleExists(ConstantVariables.studentsRole))
+            {
+                role = new IdentityRole();
+                role.Name = ConstantVariables.studentsRole;
+                roleManager.Create(role);
+            }
+        }
+
+        public void CreateDefaultAdminUsers()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = ConstantVariables.adminUserName,
+                Email = ConstantVariables.adminEamil
+            };
+            var result = userManager.Create(user, ConstantVariables.adminDefaultPassword);
+            if (result.Succeeded)
+            {
+                userManager.AddToRole(user.Id, ConstantVariables.adminsRole);
+            }
+
+        }
+
+
+    }
+}
